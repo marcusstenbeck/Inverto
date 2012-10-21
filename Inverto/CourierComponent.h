6c45145d5a65dbc8ddee2a7ba07de1f757d9a9c1
Marcus Stenbeck <marcus.stenbeck@gmail.com>
2012-10-21 18:24:47 +0200

    Added .gitignore

---
+++ Inverto/CourierComponent.h
@@ -0,0 +1,16 @@
+//
+//  CourierComponent.h
+//  Inverto
+//
+//  Created by Marcus Stenbeck on 2012-10-21.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CourierComponent : GameComponent {
+    
+}
+
+@end
---
+++ Inverto/CourierComponent.m
@@ -0,0 +1,14 @@
+//
+//  CourierComponent.m
+//  Inverto
+//
+//  Created by Marcus Stenbeck on 2012-10-21.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CourierComponent.h"
+
+
+@implementation CourierComponent
+
+@end
